b10272b40634ef99abddddb962b6b3136803bc95
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2023-12-05 23:17:43 +0700

    Membuat Destinasi Navigasi

---
+++ app/src/main/java/com/example/room_database/navigasi/DestinasiNavigasi.kt
@@ -0,0 +1,14 @@
+package com.example.room_database.navigasi
+
+interface DestinasiNavigasi {
+    /*
+    Nama unik untuk menentukan jalur untuk composable
+     */
+
+    val route: String
+
+    /*
+    String resource id yang berisi judul yang akan ditampilkan di layar halaman
+     */
+    val titleRes: Int
+}
