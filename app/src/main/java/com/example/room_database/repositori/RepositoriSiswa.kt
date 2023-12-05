43fee4c658f50738dcfc12066e4e0062341dde03
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2023-12-05 22:25:41 +0700

    membuat interface repos siswa

--- app/src/main/java/com/example/room_database/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/room_database/repositori/RepositoriSiswa.kt
@@ -1,4 +1,17 @@
 package com.example.room_database.repositori
 
+import com.example.room_database.data.Siswa
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+
+    suspend fun insertSiswa(siswa: Siswa)
+
+    suspend fun deleteSiswa(siswa: Siswa)
+
+    suspend fun updateSiswa(siswa: Siswa)
+
 }
