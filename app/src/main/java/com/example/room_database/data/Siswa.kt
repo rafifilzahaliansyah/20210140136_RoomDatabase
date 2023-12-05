5f07144672de8faaf9337bd53b41f4b7ec451cc5
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2023-12-05 21:46:01 +0700

    membuat data class siswa

--- app/src/main/java/com/example/room_database/data/Siswa.kt
+++ app/src/main/java/com/example/room_database/data/Siswa.kt
@@ -1,4 +1,13 @@
 package com.example.room_database.data
 
-class Siswa {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telepon : String,
+)
