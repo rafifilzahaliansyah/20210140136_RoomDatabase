a8c7bca3ea28e25d33a8c178be5005f6cfed1739
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2023-12-05 22:07:43 +0700

    membuat interface containerapp

--- app/src/main/java/com/example/room_database/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/room_database/repositori/ContainerApp.kt
@@ -1,6 +1,14 @@
 package com.example.room_database.repositori
 
+import android.content.Context
+import com.example.room_database.data.DatabaseSiswa
+
 interface ContainerApp {
     val repositoriSiswa : RepositoriSiswa
+}
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositorySiswa(DatabaseSiswa.getDatabase(context).SiswaDao())
+    }
 }
 
