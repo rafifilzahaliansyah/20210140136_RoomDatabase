e550571dc19b7be4fa952b9c3dde736afd574569
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2023-12-05 22:17:35 +0700

    membuat class OfflineRepositorySiswa

--- app/src/main/java/com/example/room_database/repositori/OfflineRepositorySiswa.kt
+++ app/src/main/java/com/example/room_database/repositori/OfflineRepositorySiswa.kt
@@ -1,4 +1,28 @@
 package com.example.room_database.repositori
 
-class OfflineRepositorySiswa {
+import com.example.room_database.data.Siswa
+import com.example.roomsiswa.Data.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+class OfflineRepositorySiswa (private val siswaDao: SiswaDao) : RepositoriSiswa {
+
+    override fun getAllSiswaStream(): Flow<List<Siswa>> {
+        return siswaDao.getAllSiswa()
+
+    }
+    override fun getSiswaStream(id: Int): Flow<Siswa?> {
+        return siswaDao.getSiswa(id)
+    }
+    override suspend fun insertSiswa(siswa: Siswa) {
+        return siswaDao.insert(siswa)
+    }
+
+    override suspend fun deleteSiswa(siswa: Siswa) {
+        return siswaDao.delete(siswa)
+    }
+
+    override suspend fun updateSiswa(siswa: Siswa) {
+        return siswaDao.update(siswa)
+    }
+
 }
