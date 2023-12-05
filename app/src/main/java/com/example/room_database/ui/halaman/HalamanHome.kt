36820044c95e0e955b88fc3f224c7a27e6c15301
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2023-12-06 00:31:42 +0700

    memperbaiki halaman home

--- app/src/main/java/com/example/room_database/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/room_database/ui/halaman/HalamanHome.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -26,6 +27,7 @@ import com.example.room_database.data.Siswa
 import com.example.room_database.model.HomeViewModel
 import com.example.room_database.model.PenyediaViewModel
 import com.example.room_database.navigasi.DestinasiNavigasi
+import com.example.room_database.navigasi.SiswaTopAppBar
 
 
 object DestinasiHome : DestinasiNavigasi {
